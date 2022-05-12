5f2c1ec74586c3f73125abc93c8e57181ebe9f86
chinmayashukla <83912177+chinmayashukla@users.noreply.github.com>
2022-05-12 12:10:35 +0530

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,45 @@
+import random
+import hangman_art
+import hangman_words
+
+
+def win():
+    print(f"The word has been identified: {guesing_word}\nCongratulations - You Win")
+    exit()
+
+
+def lose():
+    print("Sorry, you have no attempts left - you loose")
+    print(f"The answer was: {guesing_word}")
+    exit()
+
+
+print(hangman_art.logo)
+guessing_word_list = random.choice(hangman_words.word_list)
+guesing_word = guessing_word_list
+guess_list = list(guesing_word)
+word_length = len(guess_list)
+display_list = []
+missed_letters = []
+attempts = 7
+
+for i in range(0, word_length):
+    display_list.append("_")
+
+while attempts != 0:
+    scr_input = input("Please enter your guess:\n").lower()
+    if scr_input in guess_list:
+        for i in range(0, word_length):
+            if scr_input == guess_list[i]:
+                display_list[i] = scr_input
+        if "_" not in display_list:
+            win()
+    else:
+        attempts -= 1
+        missed_letters.append(scr_input)
+        print("You missed - a life has been deducted")
+        print(hangman_art.stages[attempts])
+        if attempts == 0:
+            lose()
+    print(f"Current Word Status: {display_list}")
+    print(f"Missed Letters = {missed_letters}\nAttempts remaining = {attempts}")
